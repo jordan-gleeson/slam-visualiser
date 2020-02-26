cba28f7f7c4b288986b2c37b57c42f564116ce20
jordan-gleeson <jordangleeson@gmail.com>
2020-02-26 17:29:35 +1100

    Added initial pylint fixes.

--- slam_visualiser.py
+++ slam_visualiser.py
@@ -1,56 +1,57 @@
 #Tutorial : how to use ThorPy with a pre-existing code - step 1
-import pygame, thorpy
+import pygame
+import thorpy
 
 class Robot:
-    def __init__(self, screen):
-        self.screen = screen
-        self.x = 10
-        self.y = 10
+    def __init__(self, p_screen):
+        self.screen = p_screen
+        self.x_pos = 10
+        self.y_pos = 10
         self.img = pygame.image.load("roomba.jpg")
         self.velocity = 2  # pixels/input
 
     def update(self):
-        self.screen.blit(self.img, (self.x, self.y))
+        self.screen.blit(self.img, (self.x_pos, self.y_pos))
 
     def move_distance(self, distance, direction):
         if direction == "DOWN":
-            if self.y < self.screen.get_height() - self.get_size()[1]:
-                self.y += distance
+            if self.y_pos < self.screen.get_height() - self.get_size()[1]:
+                self.y_pos += distance
         elif direction == "UP":
-            if self.y > 0:
-                self.y -= distance
+            if self.y_pos > 0:
+                self.y_pos -= distance
         elif direction == "LEFT":
-            if self.x > 0:
-                self.x -= distance
+            if self.x_pos > 0:
+                self.x_pos -= distance
         elif direction == "RIGHT":
-            if self.x < self.screen.get_width() - self.get_size()[0]:
-                self.x += distance
+            if self.x_pos < self.screen.get_width() - self.get_size()[0]:
+                self.x_pos += distance
 
     def get_size(self):
         return (50, 50)
 
 pygame.init()
 pygame.key.set_repeat(300, 30)
-screen = pygame.display.set_mode((400,400))
-screen.fill((255,255,255))
+screen = pygame.display.set_mode((400, 400))
+screen.fill((255, 255, 255))
 rect = pygame.Rect((0, 0, 50, 50))
 rect.center = screen.get_rect().center
 clock = pygame.time.Clock()
 
-pygame.draw.rect(screen, (255,0,0), rect)
+pygame.draw.rect(screen, (255, 0, 0), rect)
 pygame.display.flip()
 
 #declaration of some ThorPy elements ...
 slider = thorpy.SliderX(100, (12, 35), "My Slider")
 button = thorpy.make_button("Quit", func=thorpy.functions.quit_func)
-box = thorpy.Box(elements=[slider,button])
+box = thorpy.Box(elements=[slider, button])
 #we regroup all elements on a menu, even if we do not launch the menu
 menu = thorpy.Menu(box)
 #important : set the screen as surface for all elements
 for element in menu.get_population():
     element.surface = screen
 #use the elements normally...
-box.set_topleft((100,100))
+box.set_topleft((100, 100))
 box.blit()
 box.update()
 
@@ -64,7 +65,7 @@ while playing_game:
         if event.type == pygame.QUIT:
             playing_game = False
             break
-        elif event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
                 robot.move_distance(1 * robot.velocity, "DOWN")
             elif event.key == pygame.K_UP:
