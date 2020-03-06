bf7ee580e23298e34740a296e5594f62625ab7a4
jordan-gleeson <jordangleeson@gmail.com>
2020-03-06 14:09:20 +1100

    Started converting world and laser to sprites for mask collision.

--- slam_visualiser.py
+++ slam_visualiser.py
@@ -54,19 +54,19 @@ class Robot:
 
     def move_velocity(self):
         deceleration = self.acceleration / 2
-        collision_side = self.collision_detector()
-        if collision_side == "TOP":
-            if self.velocity[1] < 0:
-                self.velocity[1] = 0
-        elif collision_side == "BOTTOM":
-            if self.velocity[1] > 0:
-                self.velocity[1] = 0
-        elif collision_side == "RIGHT":
-            if self.velocity[0] > 0:
-                self.velocity[0] = 0
-        elif collision_side == "LEFT":
-            if self.velocity[0] < 0:
-                self.velocity[0] = 0
+        # collision_side = self.collision_detector()
+        # if collision_side == "TOP":
+        #     if self.velocity[1] < 0:
+        #         self.velocity[1] = 0
+        # elif collision_side == "BOTTOM":
+        #     if self.velocity[1] > 0:
+        #         self.velocity[1] = 0
+        # elif collision_side == "RIGHT":
+        #     if self.velocity[0] > 0:
+        #         self.velocity[0] = 0
+        # elif collision_side == "LEFT":
+        #     if self.velocity[0] < 0:
+        #         self.velocity[0] = 0
 
         self.x_pos += self.velocity[0]
         self.y_pos += self.velocity[1]
@@ -181,33 +181,57 @@ class Robot:
         lidar.xy = (self.x_pos, self.y_pos)
         lasers = []
         for i in range(self.point_count):
-            if i >= 0:
+            if i == 43:
                 degree_multiplier = 365 / self.point_count
                 cur_angle = i * degree_multiplier
+                laser = pygame.sprite.Sprite()
                 laser = Vector2()
                 laser.from_polar((100, cur_angle))
                 rect = pygame.draw.aaline(
                     self.screen, (255, 0, 0), lidar, lidar + laser)
-                
+                # laser_sprite = Laser(self.screen, rect, rect.left, rect.top, rect.width, rect.height)
+                laser_sprite = Laser(self.screen, lidar, lidar + laser)
+                olist = laser_sprite.mask.outline()
+                print(olist)
+                # pygame.draw.lines(screen, (150, 255, 0), 1, olist)
                 heading_direction = laser.normalize()
-                obs_found = False
                 collide_point = (-1, -1)
                 # TODO: Find the closest collide point (SUPER SLOW???)
                 closest_point = (-1, -1)
-                for j in range(len(self.world.wall_list)):
-                    cur_pos = Vector2(self.x_pos, self.y_pos)
-                    for _ in range(int(laser.length())):
-                        cur_pos += heading_direction
+                for j in range(len(self.world.wall_list.sprites())):
+                    # cur_pos = Vector2(self.x_pos, self.y_pos)
+                    # for _ in range(int(laser.length())):
+                    #     cur_pos += heading_direction
                         # pygame.draw.circle(self.screen, (0, 0, 255), (int(cur_pos.x), int(cur_pos.y)), 1)
-                        if self.world.wall_list[j].collidepoint(cur_pos):
-                            collide_point = (cur_pos.x, cur_pos.y)
-                if not collide_point == (-1, -1):
-                    pygame.draw.circle(self.screen, (0, 0, 255), (int(collide_point[0]), int(collide_point[1])), 5)
-        print()
+                    # print(self.world.wall_list.sprites()[j])
+                    if not pygame.sprite.collide_mask(laser_sprite, self.world.wall_list.sprites()[j]) == None:
+                        # print("collision")
+                        pass
+                        # collide_point = (cur_pos.x, cur_pos.y)
+                # if not collide_point == (-1, -1):
+                #     print(collide_point)
+                #     pygame.draw.circle(self.screen, (0, 0, 255), (int(collide_point[0]), int(collide_point[1])), 5)
+        # print()
         # laser.from_polar((50, i * degree_multiplier))
         # rect = pygame.draw.aaline(self.screen, (255, 0, 0), lidar, lidar + laser)
         # laser = Vector2()
         # laser.from_polar((50, -self.direction + 90))
+
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, p_screen, left, top, width, height):
+        pygame.sprite.Sprite.__init__(self)
+        self.rect = pygame.Rect(left, top, width, height)
+        self.image = pygame.Surface((width, height))
+        # pygame.draw.rect(self.image, (0, 0, 0), self.rect)
+        
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, p_screen, start, end): #image, left, top, width, height):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface(p_screen.get_size())
+        self.rect = pygame.draw.aaline(self.image, (255, 0, 0), start, end)
+        self.mask = pygame.mask.from_threshold(self.image, 250)
+        # print(self.mask)
 
 
 class World():
@@ -216,12 +240,13 @@ class World():
         self.size = 5
         self.grid = [[0 for _ in range(self.screen.get_size()[0] // 5)]
                      for __ in range(self.screen.get_size()[1] // 5)]
-        self.wall_list = []
+        self.wall_list = pygame.sprite.RenderPlain()
         self.write_map()
+        self.create_sprites()
 
     def write_map(self):
         # Drawing map
-        for i in range(len(self.grid[0])):
+        for i in range(len(self.grid)):
             for j in range(len(self.grid[0])):
                 if i == 0 or i == len(self.grid) - 1 or j == 0 or j == len(self.grid[0]) - 1:
                     self.grid[i][j] = 1
@@ -231,19 +256,24 @@ class World():
                     if j > 25 and j < 30:
                         self.grid[i][j] = 1
 
-    def draw(self):
-        self.wall_list = []
+    def create_sprites(self):
+        self.wall_list.empty()
         for i in range(len(self.grid)):
             for j in range(len(self.grid[0])):
                 if self.grid[i][j]:
-                    wall_rect = pygame.Rect(i * self.size,
-                                            j * self.size,
-                                            self.size,
-                                            self.size)
-                    self.wall_list.append(wall_rect)
-                    pygame.draw.rect(screen,
-                                     (0, 0, 0),
-                                     wall_rect)
+                    wall_rect = Wall(screen,
+                                     i * self.size,
+                                     j * self.size,
+                                     self.size,
+                                     self.size)
+                    self.wall_list.add(wall_rect)
+        print(self.wall_list)
+
+    def draw(self):
+        self.wall_list.draw(self.screen)
+        # self.wall_list.RenderUpdates
+        # pygame.display.update()
+
 
 
 pygame.init()
